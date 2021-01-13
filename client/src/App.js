2fd0826a12b9aec9d646e6223ba59cb50e619c01
Bethanie Innes <doloresdadd@gmail.com>
2021-01-13 15:58:46 +1100

    initialise redux store

--- client/src/App.js
+++ client/src/App.js
@@ -5,30 +5,36 @@ import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
-
-import "./App.scss";
 import Quiz from "./components/quiz/Quiz";
 import CreateQuiz from "./components/quiz/CreateQuiz";
 import FindQuiz from "./components/quiz/FindQuiz";
 
+//Redux
+import { Provider } from "react-redux";
+import store from "./store";
+
+import "./App.scss";
+
 const App = () => {
   return (
-    <Router>
-      <Fragment>
-        <Navbar />
-        <section className="container center-align">
-          <Route exact path="/" component={Landing} />
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <section className="container center-align">
+            <Route exact path="/" component={Landing} />
 
-          <Switch>
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/quiz" component={Quiz} />
-            <Route exact path="/createquiz" component={CreateQuiz} />
-            <Route exact path="/quizzes" component={FindQuiz} />
-          </Switch>
-        </section>
-      </Fragment>
-    </Router>
+            <Switch>
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/quiz" component={Quiz} />
+              <Route exact path="/createquiz" component={CreateQuiz} />
+              <Route exact path="/quizzes" component={FindQuiz} />
+            </Switch>
+          </section>
+        </Fragment>
+      </Router>
+    </Provider>
   );
 };
 
